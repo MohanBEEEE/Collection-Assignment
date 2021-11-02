68e92325ec3096f0df3c883a6431512ddc311bb2
MohanBEEEE <mohanshai1998@gmail.com>
2021-11-02 18:39:24 +0530

    it is the 1st file

---
+++ Students.java
@@ -0,0 +1,29 @@
+package com.te.practiceweek;
+
+import java.util.Comparator;
+
+public class Students implements Comparator<Student> {
+	int id;
+	String name;
+	double per;
+	int age;
+	
+	public Students(int id, String name, double per, int age) {
+		
+		this.id = id;
+		this.name = name;
+		this.per = per;
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "[ id=" + id + ", name=" + name + ", per=" + per + ", age=" + age + "]";
+	}
+	
+	@Override
+	public int compare(Student o1, Student o2) {
+		return o1.age-o2.age;
+		//return o1.id-o2.id;
+	}	
+}
